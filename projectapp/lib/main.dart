499a581e377124be2a591ca6e8276762511bc93f
MOHAMMED ASLAM <aslam.mohd14338@gmail.com>
2024-11-13 14:41:15 -0500

    changged name to Quick Tema up in home page

--- projectapp/lib/main.dart
+++ projectapp/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Quick Team Up'),
     );
   }
 }
